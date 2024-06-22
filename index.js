9ca8fdb07b444d8be2ffb768c779bc14b96ed03b
auberdjinn <auberdjinn@proton.me>
2024-06-21 21:14:49 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+function findMatching(names, filter) {
+  return names.filter(function (val) {
+    return val.toLowerCase() === filter.toLowerCase();
+  });
+}
+
+function fuzzyMatch(names, filter) {
+  return names.filter(function (val) {
+    return val.startsWith(filter);
+  });
+}
+
+function matchName(names, filter) {
+  return names.filter(function (driver) {
+    return driver.name === filter;
+  });
+}
